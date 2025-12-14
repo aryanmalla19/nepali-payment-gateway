05dd812613138944e86b271a71bf54911fb5349f
Aryan Malla <aryanmalla19@gmail.com>
2025-12-14 16:45:03 +0545

    added curl http client

--- src/Http/CurlHttpClient.php
+++ src/Http/CurlHttpClient.php
@@ -5,17 +5,104 @@ declare(strict_types=1);
 namespace Aryan\NepaliPaymentGateway\Http;
 
 use Aryan\NepaliPaymentGateway\Contracts\HttpClientInterface;
+use Aryan\NepaliPaymentGateway\Exceptions\HttpClientException;
 
 class CurlHttpClient implements HttpClientInterface
 {
+    private array $headers = [
+        'Content-Type: application/json',
+        'Accept: application/json',
+    ];
 
-    public function get(string $url, array $payload = [], array $headers = [])
+    /**
+     * @throws HttpClientException
+     */
+    public function get(string $url, array $payload = [], array $headers = []): array
     {
-        // TODO: Implement get() method.
+        if(!empty($payload)){
+            $url .= '?' . http_build_query($payload);
+        }
+
+        $ch = curl_init($url);
+
+        if($ch === false){
+            throw new HttpClientException('Failed to initialize cURL');
+        }
+
+        curl_setopt_array($ch, [
+            CURLOPT_HTTPGET => true,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_TIMEOUT => 10,
+            CURLOPT_CONNECTTIMEOUT => 10,
+            CURLOPT_SSL_VERIFYPEER => true,
+            CURLOPT_SSL_VERIFYHOST => 2,
+            CURLOPT_HTTPHEADER => array(
+                ...$this->headers,
+                ...$headers,
+            ),
+        ]);
+
+        return $this->executeRequest($ch);
     }
 
-    public function post(string $url, array $payload = [], array $headers = [])
+    /**
+     * @throws HttpClientException
+     */
+    public function post(string $url, array $payload = [], array $headers = []): array
     {
-        // TODO: Implement post() method.
+        $ch = curl_init($url);
+
+        if($ch === false){
+            throw new HttpClientException('Failed to initialize cURL');
+        }
+
+        curl_setopt_array($ch, [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_TIMEOUT => 10,
+            CURLOPT_CONNECTTIMEOUT => 10,
+            CURLOPT_SSL_VERIFYPEER => true,
+            CURLOPT_SSL_VERIFYHOST => 2,
+            CURLOPT_HTTPHEADER => array(
+                ...$this->headers,
+                ...$headers,
+            ),
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => json_encode($payload),
+        ]);
+
+        return $this->executeRequest($ch);
+    }
+
+    /**
+     * @throws HttpClientException
+     */
+    private function executeRequest($ch)
+    {
+        try {
+            $response = curl_exec($ch);
+
+            if($response === false){
+                $errno = curl_errno($ch);
+                $error = curl_error($ch);
+
+                throw new HttpClientException("cURL transport error ({$errno}) $error");
+            }
+
+            $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+            if($httpCode >= 400){
+                throw new HttpClientException("HTTP error {$httpCode}: {$response}");
+            }
+
+            $decode = json_decode($response, true);
+
+            if(json_last_error() !== JSON_ERROR_NONE){
+                throw new HttpClientException('Could not parse into JSON format: '. json_last_error_msg());
+            }
+
+            return $decode;
+        } finally {
+            curl_close($ch);
+        }
     }
 }
