f2a4d6f82f1cd173108277e162a91601e6eea4fd
Aryan Malla <aryanmalla19@gmail.com>
2025-12-15 21:24:27 +0545

    added esewa test

--- tests/Feature/PaymentTest.php
+++ tests/Feature/PaymentTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use Kbk\NepaliPaymentGateway\Contracts\BasePaymentVerifyResponse;
 use Kbk\NepaliPaymentGateway\Epay\Esewa;
 use Kbk\NepaliPaymentGateway\Exceptions\InvalidPayloadException;
 use PHPUnit\Framework\TestCase;
@@ -52,5 +53,95 @@ class PaymentTest extends TestCase
             'success_url' => 'https://example.com/success',
             'failure_url' => 'https://example.com/failure',
         ]);
+    }
+
+    public function test_it_should_throw_an_exception_when_success_url_is_missing()
+    {
+        $this->expectException(InvalidPayloadException::class);
+        $esewa = new Esewa('EPAYTEST', '3282733');
+        $this->expectExceptionMessage('Invalid success URL');
+        $esewa->payment([
+            'total_amount' => 100,
+            'transaction_uuid' => uniqid(),
+            'failure_url' => 'https://example.com/failure',
+        ]);
+    }
+
+    public function test_it_should_throw_an_exception_when_failure_url_is_missing()
+    {
+        $this->expectException(InvalidPayloadException::class);
+        $this->expectExceptionMessage('Invalid failure URL');
+        $esewa = new Esewa('EPAYTEST', '3282733');
+        $esewa->payment([
+            'total_amount' => 100,
+            'transaction_uuid' => uniqid(),
+            'success_url' => 'https://example.com/success',
+        ]);
+    }
+
+    public function test_it_should_throw_an_exception_when_success_url_is_invalid()
+    {
+        $this->expectException(InvalidPayloadException::class);
+        $this->expectExceptionMessage('Invalid success URL');
+        $esewa = new Esewa('EPAYTEST', '3282733');
+        $esewa->payment([
+            'total_amount' => 100,
+            'transaction_uuid' => uniqid(),
+            'success_url' => 'It should fail',
+            'failure_url' => 'https://example.com/failure',
+        ]);
+    }
+
+    public function test_it_should_throw_an_exception_when_total_amount_is_non_numeric()
+    {
+        $this->expectException(InvalidPayloadException::class);
+        $this->expectExceptionMessage('Total Amount is required and must be a numeric value.');
+        $esewa = new Esewa('EPAYTEST', '3282733');
+        $esewa->verify([
+            'totalAmount' => -1,
+            'transactionUuid' => uniqid(),
+        ]);
+    }
+
+    public function test_it_should_throw_an_exception_when_total_amount_is_non_numeric_in_validation()
+    {
+        $this->expectException(InvalidPayloadException::class);
+        $this->expectExceptionMessage('Total Amount is required and must be a numeric value.');
+        $esewa = new Esewa('EPAYTEST', '3282733');
+        $esewa->verify([
+            'totalAmount' => -1,
+            'transactionUuid' => uniqid(),
+        ]);
+    }
+
+    public function test_it_should_throw_an_exception_when_total_amount_is_missing_in_validation()
+    {
+        $this->expectException(InvalidPayloadException::class);
+        $this->expectExceptionMessage('Total Amount is required and must be a numeric value.');
+        $esewa = new Esewa('EPAYTEST', '3282733');
+        $esewa->verify([
+            'transactionUuid' => uniqid(),
+        ]);
+    }
+
+    public function test_it_should_throw_an_exception_when_transaction_uuid_is_missing_in_validation()
+    {
+        $this->expectException(InvalidPayloadException::class);
+        $this->expectExceptionMessage('Transaction Uuid is required');
+        $esewa = new Esewa('EPAYTEST', '3282733');
+        $esewa->verify([
+            'total_amount' => 100,
+        ]);
+    }
+
+    public function test_it_should_return_correct_base_verify_response_class()
+    {
+        $esewa = new Esewa('EPAYTEST', '3282733');
+        $response = $esewa->verify([
+            'total_amount' => 100,
+            'transaction_uuid' => '123',
+        ]);
+
+        $this->assertInstanceOf(BasePaymentVerifyResponse::class, $response);
     }
 }
