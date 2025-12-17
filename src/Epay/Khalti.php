c658118ec44bb2a78ad08ee222aa0fc64fd742ed
Aryan Malla <aryanmalla19@gmail.com>
2025-12-17 23:17:18 +0545

    added bankDetail and refund feat in Khalti

--- src/Epay/Khalti.php
+++ src/Epay/Khalti.php
@@ -12,7 +12,7 @@ use Kbk\NepaliPaymentGateway\DTOs\KhaltiVerifyResponseDTO;
 use Kbk\NepaliPaymentGateway\Exceptions\InvalidPayloadException;
 use Kbk\NepaliPaymentGateway\Http\CurlHttpClient;
 
-class Khalti extends BasePaymentGateway
+final class Khalti extends BasePaymentGateway
 {
     private readonly string $environment;
     private readonly string $secretKey;
@@ -20,11 +20,11 @@ class Khalti extends BasePaymentGateway
 
     const BASE_URLS = [
         'live' => [
-            'url' => 'https://khalti.com/api/v2/',
+            'url' => 'https://khalti.com/api/',
             'token' => '',
         ],
         'test' => [
-            'url' => 'https://dev.khalti.com/api/v2/',
+            'url' => 'https://dev.khalti.com/api/',
             'token' => '',
         ],
     ];
@@ -44,7 +44,7 @@ class Khalti extends BasePaymentGateway
      */
     public function payment(array $data): void
     {
-        $url = self::BASE_URLS[$this->environment]['url'] . 'epayment/initiate/';
+        $url = self::BASE_URLS[$this->environment]['url'] . 'v2/epayment/initiate/';
         $dto = KhaltiRequestDTO::fromArray($data);
 
         $response = $this->httpClient->post($url, $dto->toArray(), $this->headers);
@@ -57,7 +57,7 @@ class Khalti extends BasePaymentGateway
      */
     public function verify(array $data): BasePaymentVerifyResponse
     {
-        $url = self::BASE_URLS[$this->environment]['url'] . 'epayment/lookup/';
+        $url = self::BASE_URLS[$this->environment]['url'] . 'v2/epayment/lookup/';
 
         if(!isset($data['pidx'])){
             throw new InvalidPayloadException('Pidx is required');
@@ -66,5 +66,28 @@ class Khalti extends BasePaymentGateway
         $response = $this->httpClient->post($url, $data, $this->headers);
 
         return new KhaltiVerifyResponseDTO($response);
+    }
+
+    public function refund(string $transactionId, ?int $amount=null)
+    {
+        $url = self::BASE_URLS[$this->environment]['url'] . "merchant-transaction/{$transactionId}/refund/";
+
+        return $this->httpClient->post($url, [
+            'amount' => $amount,
+        ], $this->headers);
+    }
+
+    /**
+     * @throws InvalidPayloadException
+     */
+    public function getBankList(string $paymentType='ebanking')
+    {
+        if(!in_array($paymentType, ['ebanking', 'mobilecheckout'])){
+            throw new InvalidPayloadException('Payment Type must be ebanking or mobilecheckout');
+        }
+
+        $url = self::BASE_URLS['live']['url'] . '/v5/bank/?payment_type=' . $paymentType;
+
+        return $this->httpClient->get($url);
     }
 }
