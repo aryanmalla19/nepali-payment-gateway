1e898f005e1d27ae0378174c53804110b93d7e84
Aryan Malla <aryanmalla19@gmail.com>
2025-12-23 00:00:50 +0545

    added connect ips helper function

--- src/helpers.php
+++ src/helpers.php
@@ -10,3 +10,18 @@ if (!function_exists('esewa_signature_hash')) {
         return base64_encode($signature);
     }
 }
+
+
+
+if(! function_exists('connectips_signature_hash')) {
+    function connectips_signature_hash(array $signatureData, string $path): string
+    {
+        $message = "MERCHANTID={$signatureData['MERCHANTID']},APPID={$signatureData['APPID']},APPNAME={$signatureData['APPNAME']},TXNID={$signatureData['TXNID']},TXNDATE={$signatureData['TXNDATE']},TXNCRNCY={$signatureData['TXNCRNCY']},TXNAMT={$signatureData['TXNAMT']},REFERENCEID={$signatureData['REFERENCEID']},REMARKS={$signatureData['REMARKS']},PARTICULARS={$signatureData['PARTICULARS']},TOKEN=TOKEN";
+
+        $privateKey = openssl_pkey_get_private(file_get_contents($path));
+
+        openssl_sign($message, $signature, $privateKey, OPENSSL_ALGO_SHA256);
+
+        return base64_encode($signature);
+    }
+}
