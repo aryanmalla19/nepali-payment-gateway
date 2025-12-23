a4202e114edbfdccdeed8dd57089a1cde4b68a4b
Aryan Malla <aryanmalla19@gmail.com>
2025-12-23 21:45:56 +0545

    added validation for verify in connect ips

---
+++ src/Validators/ConnectIpsVerify.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Kbk\NepaliPaymentGateway\Validators;
+
+use Kbk\NepaliPaymentGateway\Exceptions\InvalidPayloadException;
+
+class ConnectIpsVerify
+{
+    /**
+     * @throws InvalidPayloadException
+     */
+    public static function validate(array $data): void
+    {
+        if (empty($data['transaction_amount']) || !is_numeric($data['transaction_amount'])) {
+            throw new InvalidPayloadException('Transaction Amount must be require and numeric');
+        }
+
+        if ($data['transaction_amount'] < 0) {
+            throw new InvalidPayloadException('Transaction Amount must be require and numeric');
+        }
+
+        if (empty($data['reference_id'])) {
+            throw new InvalidPayloadException('Reference Id is required');
+        }
+    }
+}
