e17bbe508178751fd7ffbb6554189a58b1d43738
Aryan Malla <aryanmalla19@gmail.com>
2025-12-23 21:44:57 +0545

    added connect ips hash helper fn for verify

--- src/helpers.php
+++ src/helpers.php
@@ -11,12 +11,23 @@ if (!function_exists('esewa_signature_hash')) {
     }
 }
 
-
-
 if(! function_exists('connectips_signature_hash')) {
     function connectips_signature_hash(array $signatureData, string $path): string
     {
         $message = "MERCHANTID={$signatureData['MERCHANTID']},APPID={$signatureData['APPID']},APPNAME={$signatureData['APPNAME']},TXNID={$signatureData['TXNID']},TXNDATE={$signatureData['TXNDATE']},TXNCRNCY={$signatureData['TXNCRNCY']},TXNAMT={$signatureData['TXNAMT']},REFERENCEID={$signatureData['REFERENCEID']},REMARKS={$signatureData['REMARKS']},PARTICULARS={$signatureData['PARTICULARS']},TOKEN=TOKEN";
+
+        $privateKey = openssl_pkey_get_private(file_get_contents($path));
+
+        openssl_sign($message, $signature, $privateKey, OPENSSL_ALGO_SHA256);
+
+        return base64_encode($signature);
+    }
+}
+
+if(! function_exists('connectips_signature_hash_verify')) {
+    function connectips_signature_hash_verify(array $signatureData, string $path): string
+    {
+        $message = "MERCHANTID={$signatureData['merchantId']},APPID={$signatureData['appId']},,REFERENCEID={$signatureData['referenceId']},TXNAMT={$signatureData['txmAmt']}";
 
         $privateKey = openssl_pkey_get_private(file_get_contents($path));
 
