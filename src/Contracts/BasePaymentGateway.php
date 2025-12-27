48a1a73d80232ffc17061ec946d7c078060a80eb
Aryan Malla <aryanmalla19@gmail.com>
2025-12-27 16:51:56 +0545

    made httpclient priavte (encapsulation)

--- src/Contracts/BasePaymentGateway.php
+++ src/Contracts/BasePaymentGateway.php
@@ -6,5 +6,5 @@ namespace Kbk\NepaliPaymentGateway\Contracts;
 
 abstract class BasePaymentGateway implements PaymentGatewayInterface
 {
-    public function __construct(public HttpClientInterface $httpClient) {}
+    public function __construct(private readonly HttpClientInterface $httpClient) {}
 }
